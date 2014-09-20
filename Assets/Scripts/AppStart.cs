269f467cdcbd1b9b284ea7b7468cfa91a172938a
Wang Weiqiang <wangwq_net@hotmail.com>
2014-09-20 17:21:22 +0800

    add water and cloud

--- Assets/Scripts/AppStart.cs
+++ Assets/Scripts/AppStart.cs
@@ -3,11 +3,22 @@ using System.Collections;
 
 public class AppStart : MonoBehaviour {
 
-	// Use this for initialization
+	
+    AsyncOperation async;
+    int progress = 0;
+
+    // Use this for initialization
 	void Start () {
+
+        Globe.sceneName = "StartUI";
+        StartCoroutine(loadScene(Globe.sceneName));
 	
 	}
-	
+    IEnumerator loadScene(string sceneName)
+    {
+        async = Application.LoadLevelAsync(sceneName);
+        yield return async;
+    }
 	// Update is called once per frame
 	void Update () {
 	
---
+++ Assets/Scripts/Global.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Globe
+{
+    public static string sceneName;
+}
